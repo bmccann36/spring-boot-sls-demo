79bcd050a930a536614700ced867b282ccb1e11e
mccann-b <brian@cedrus.digital>
2020-09-12 13:27:06 -0400

    use asynchronous spring boot initializer

--- src/main/java/com/springbootslsdemo/SpringLambdaHandler.java
+++ src/main/java/com/springbootslsdemo/SpringLambdaHandler.java
@@ -1,9 +1,8 @@
 package com.springbootslsdemo;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
-import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.internal.servlet.AwsLambdaServletContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
@@ -12,22 +11,19 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class SpringLambdaHandler implements RequestStreamHandler {
-    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-    static {
-        try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
-            // If you are using HTTP APIs with the version 2.0 of the proxy model, use the getHttpApiV2ProxyHandler
-            // method: handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Application.class);
-        } catch (ContainerInitializationException e) {
-            // if we fail here. We re-throw the exception to force another cold start
-            e.printStackTrace();
-            throw new RuntimeException("Could not initialize Spring Boot application", e);
-        }
-    }
 
+    private AwsLambdaServletContainerHandler handler;
+    public SpringLambdaHandler() throws ContainerInitializationException {
+        handler = new SpringBootProxyHandlerBuilder()
+                .springBootApplication(Application.class)
+                .defaultProxy()
+                .asyncInit()
+                .buildAndInitialize();
+    }
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
+
         handler.proxyStream(inputStream, outputStream, context);
     }
 }
